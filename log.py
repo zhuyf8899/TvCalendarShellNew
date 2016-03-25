b364ea602c0fdfea826de27d04b9c62574d20078
ZhuYifan <zhuyf8899@163.com>
2016-03-25 21:47:21 +0800

    0.0.1.2 - add log module
    add a module to take some logs,next step is use it to any where in
    program

---
+++ log.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+# -*- coding: UTF-8 -*-
+import time
+import os
+
+class Log(object):
+	def __init__(self):
+		super(Log, self).__init__()
+		self.path = "./logs"
+		if os.path.exists(self.path):
+			pass
+		else:
+			os.makedirs(self.path)
+	def takeLog(self,level,report):
+	 	fileName = time.strftime("%Y-%m-%d", time.localtime())
+	 	fileName += '.log'
+	 	fileWholePath = self.path + '/' + fileName
+	 	try:
+		 	if not os.path.exists(fileWholePath):
+		 		fileHandler = open(fileWholePath,'w')
+		 	else:
+		 		fileHandler = open(fileWholePath,'a')
+		except Exception, e:
+			print('FATAL ERROR: Opening log file error - ' + str(e))
+			fileHandler.close()
+			exit(-1)
+		try:
+			logReport = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '[' + str(level) + ']:' + str(report)
+	 		fileHandler.write(logReport+'\n')
+	 		fileHandler.close()
+		except Exception, e:
+			print('FATAL ERROR: Writing log file error - ' + str(e))
+			fileHandler.close()
+			exit(-2)
+	 	return
--- test.py
+++ test.py
@@ -2,9 +2,15 @@
 # -*- coding: UTF-8 -*-
 from config import Config
 from readHtml import Reader
+from log import Log
 
 config = Config()
 reader = Reader(config)
+log = Log()
+log.takeLog('ERROR',"We have a test here.")
+log.takeLog('WARNING',"it is a warning.")
+log.takeLog('INFO',"it is an info.")
 
-reader.testerInEpisode()
+#reader.testerInEpisode()
+
 print("FUCK")
