c1c182613b3b5f6046f130123a275bc34104b24d
ZhuYifan <zhuyf8899@163.com>
2016-03-26 21:31:58 +0800

    0.0.1.5 - report a bug
    -report a bug in test.py but not fix yet

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python
 # -*- coding: UTF-8 -*-
 #test.py:用于调试的测试程序
+#note:有一个bug，出现在源中存在12：30pm，此时不能正确处理时间，复现步骤：slect * from episode where s_id = 7
 from config import Config
 from readHtml import Reader
 from log import Log
